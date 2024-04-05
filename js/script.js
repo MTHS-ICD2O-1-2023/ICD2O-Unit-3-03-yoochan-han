3f9a369360bd0ed97ac1f9dc10dc7ab75aa8bbdf
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-05 18:48:04 +0000

    added JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function caculate(signal) {
+  let pi_num = 3.14159265359
+  globalThis(radius)
+  let pi_chr = "π"
+  let 
+
+}
