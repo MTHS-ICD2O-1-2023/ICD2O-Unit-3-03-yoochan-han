71cbe9bab6f75982ff2c7c1687a84f3e0a6780b2
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-09 17:29:41 +0000

    #

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
-function caculate(signal) {
-  let pi_num = 3.14159265359
-  globalThis(radius)
-  let pi_chr = "π"
-  let 
-
+function Case1() {
+  const length = parseFloat(document.getElementById("get_length").value)
+  const height = parseFloat(document.getElementById("get_height").value)
+  let ans = (length * height)
+  document.getElementById("Ans").innerHTML = ans + "cm²"
+}
+function Case2() {
+  const pi = "π"
+  const radius = parseFloat(document.getElementById("get_radius").value)
+  const height = parseFloat(document.getElementById("get_height").value)
+  let ans = (length * height)
+  document.getElementById("Ans").innerHTML = ans + "cm²"
 }
