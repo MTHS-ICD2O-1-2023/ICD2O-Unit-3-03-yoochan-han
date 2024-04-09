25866cd95c16ff9b98df0589f4b2c4967b8e95db
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-09 20:05:04 +0000

    added 3 test cases

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@
 function CaseTest() {
   const pi = "π"
   const radius = parseFloat(document.getElementById("get_radius").value)
-  let ans = 4/3*(radius**3)
-  document.getElementById("Ans").innerHTML = ans + pi + "cm³"
+  let ans = 4/3*((radius**3).toFixed(2))
+  document.getElementById("progress1").innerHTML = "V = 4/3πr³(cm)"
+  document.getElementById("progress2").innerHTML = "V = 4/3" + pi + radius + "³(cm)"
+  document.getElementById("progress3").innerHTML = "V = 4/3" + pi + (radius**3).toFixed(2) + "(cm³)"
+  document.getElementById("Ans").innerHTML = "V = " + ans.toFixed(2) + pi + "cm³"
 }
